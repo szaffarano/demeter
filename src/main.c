2579112cefc6c5e3324c0cb3c4bdb5c48cfbd3e1
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2014-06-07 20:49:38 -0300

    Agrega soporte FatFS

--- src/main.c
+++ src/main.c
@@ -9,11 +9,18 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <uart/uart.h>
+#include <string.h>
 
 #include <stdio.h>
+
+#include <ff/ff.h>
+
 #define LED_DDR		DDRB
 #define	LED_PORT	PORTB
-#define	LED			PB1
+#define	LED			PB4
+
+FATFS fs;
+FIL log_file;
 
 /**
  * MCU: Atmega328
@@ -32,15 +39,38 @@ int main(void) {
 		LED_PORT ^= _BV(LED);
 		_delay_ms(100);
 	}
+	LED_PORT &= ~_BV(LED);
 
+	UINT bw;
+
+	printf("Montando FS...\r\n");
+	f_mount(&fs, "", 0);
+	char buff[100];
+	for (i = 0; i < 10; i++) {
+		if (f_open(&log_file, "datos.log", FA_OPEN_ALWAYS | FA_WRITE | FA_READ)
+				== FR_OK) {
+			f_lseek(&log_file, f_size(&log_file));
+			sprintf(buff, "Hola mundo %d\n", i);
+			f_write(&log_file, buff, strlen(buff), &bw);
+			f_close(&log_file);
+			if (bw == strlen(buff)) {
+				LED_PORT |= _BV(LED);
+			} else {
+				printf("No se pudo escribir en el archivo\r\n");
+			}
+		} else {
+			printf("No se pudo abrir el archivo.\r\n");
+		}
+
+	}
 	while (1) {
 		unsigned char r = uart_getc();
 		switch (r) {
 		case 'a':
-			printf("Letra A\r\n");
+			printf(".Letra A\r\n");
 			break;
 		case 'b':
-			printf("Letra B\r\n");
+			printf(".Letra B\r\n");
 			break;
 		case 0:
 			break;
@@ -50,4 +80,14 @@ int main(void) {
 	}
 
 	return (0);
+}
+
+DWORD get_fattime(void) {
+	/* Returns current time packed into a DWORD variable */
+	return ((DWORD) (2013 - 1980) << 25) /* Year 2013 */
+	| ((DWORD) 7 << 21) /* Month 7 */
+	| ((DWORD) 28 << 16) /* Mday 28 */
+	| ((DWORD) 0 << 11) /* Hour 0 */
+	| ((DWORD) 0 << 5) /* Min 0 */
+	| ((DWORD) 0 >> 1); /* Sec 0 */
 }
