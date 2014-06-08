aa6aad489efddf9e71777e335a76aa0520fa57a1
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2014-06-08 18:18:41 -0300

    Incorpora librería de temporizadores

---
+++ lib/timer/timer.c
@@ -0,0 +1,33 @@
+/*
+ * timer.c
+ *
+ *  Created on: 03/06/2014
+ *      Author: sebas
+ */
+#include <sandbox.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "timer.h"
+#include <stdio.h>
+
+static volatile callback_t callback;
+
+void timer1_init(callback_t cb) {
+	// Seteo del temporizador: preescaler de 1024 y el registro
+	// TCNT1 = 15625 se cuantifica 1 segundo, que sera la
+	// granularidad manejada.
+	TCCR1A = 0;    // Timer1 en normal port operation.
+	TCCR1B = ((1 << CS12) | (1 << CS10)); // prescaler en 1024
+	TCCR1B |= (1 << WGM12); // seteo para CTC (clear time on compare match)
+	OCR1A = 15624; // tics para que pase un segundo segun el prescaler en 1024
+	TIMSK1 |= (1 << OCIE1A); // bit para habilitar CTC interrupt
+
+	callback = cb;
+}
+
+ISR(TIMER1_COMPA_vect) {
+	if (callback != NULL) {
+		callback();
+	}
+}
+
---
+++ lib/timer/timer.h
@@ -0,0 +1,15 @@
+/*
+ * timer.h
+ *
+ *  Created on: 03/06/2014
+ *      Author: sebas
+ */
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+typedef void(*callback_t)(void);
+
+void timer1_init(callback_t callback);
+
+#endif /* TIMER_H_ */
